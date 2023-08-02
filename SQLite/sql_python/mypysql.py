551fa60db6d614e5cfb79a57a859957eb38290ca
Joao-Inacio <joaoinacio206@protonmail.com>
2023-08-02 10:11:40 -0300

    TRUNCATE e INSERT p/ limpar e criar valores na tabela com um ou mais cursores

--- SQLite/sql_python/mypysql.py
+++ SQLite/sql_python/mypysql.py
@@ -5,6 +5,8 @@ import pymysql
 
 dotenv.load_dotenv()
 
+TABLE_NAME = 'usuarios'
+
 connection = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
     user=os.environ['MYSQL_USER'],
@@ -16,12 +18,19 @@ with connection:
     with connection.cursor() as cursor:
         # SQL
         cursor.execute(
-            'CREATE TABLE IF NOT EXISTS usuarios ('
+            F'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
             'id INT NOT NULL AUTO_INCREMENT, '
             'nome VARCHAR(50) NOT NULL, '
             'idade INT NOT NULL , '
             'PRIMARY KEY (id)'
             ') '
         )
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')  # Limpa a tabela
         connection.commit()
         print(cursor)
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f"INSERT INTO {TABLE_NAME} "
+            "(nome, idade) VALUES ('Luiz', 24)"
+        )
+        connection.commit()
